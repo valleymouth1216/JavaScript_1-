ca0690d956e2d4e979389c16fc500076a8a55a25
valleymouth1216 <104252479+valleymouth1216@users.noreply.github.com>
2022-12-24 23:14:15 +0900

    20221224

--- app.js
+++ app.js
@@ -16,3 +16,23 @@ alert(2 * 6);
 
 // 割り算
 alert(10 / 2);
+
+
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+
+
+alert(int1);
+alert(int2);
+alert(float1);
+alert(str1);
